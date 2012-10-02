86cfb4ce14b17a138b25d182c84cef9cc4f2863c
Viet-Tran Nguyen <viettran@mit.edu>
2012-10-02 03:36:13 -0400

    Problem 2 commit

--- src/differentiator/Lexer.java
+++ src/differentiator/Lexer.java
@@ -1,17 +1,73 @@
 package differentiator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * A lexer takes a string and splits it into tokens that are meaningful to a
  * parser.
  */
 public class Lexer {
+    private final String lex;
+
     /**
      * Creates the lexer over the passed string.
-     * @param string The string to tokenize.
+     * 
+     * @param string
+     *            The string to tokenize.
      */
     public Lexer(String string) {
-        // TODO write me
+        this.lex = string;
     }
 
+    public ArrayList<Token> lexAnalysis() {
+        ArrayList<Token> tokens = new ArrayList<Token>();
+        StringBuffer buffer = new StringBuffer();
+        for (Token.Type type : Token.Type.values()) {
+            buffer.append(type.getPattern() + "|");
+        }             
+        Pattern pattern = Pattern.compile(new String(buffer));
+        Matcher matcher = pattern.matcher(lex);
+        while (matcher.find()) {
+            if (matcher.group().matches(Token.Type.INTEGER.getPattern())) {
+                tokens.add(new Token(Token.Type.INTEGER, matcher.group()));
+                continue;
+            } else if (matcher.group().matches(Token.Type.FLOAT.getPattern())) {
+                tokens.add(new Token(Token.Type.FLOAT, matcher.group()));
+                continue;
+                // } else if
+                // (matcher.group().matches(Token.Type.PLUS.getPattern())) {
+                // tokens.add(new Token(Token.Type.PLUS, matcher.group()));
+                // continue;
+            } else if (matcher.group().matches(
+                    Token.Type.OPERATION.getPattern())) {
+                tokens.add(new Token(Token.Type.OPERATION, matcher.group()));
+                continue;
+            } else if (matcher.group()
+                    .matches(Token.Type.VARIABLE.getPattern())) {
+                tokens.add(new Token(Token.Type.VARIABLE, matcher.group()));
+                continue;
+            } else if (matcher.group().matches(
+                    Token.Type.WHITESPACE.getPattern())) {
+                tokens.add(new Token(Token.Type.WHITESPACE, matcher.group()));
+                continue;
+            } else if (matcher.group().matches(Token.Type.PAREN.getPattern())) {
+                tokens.add(new Token(Token.Type.PAREN, matcher.group()));
+                continue;
+            }
+
+        }
+        return tokens;
+    }
+
+    public static void main(String[] args) {
+        Lexer lexer = new Lexer("((3 * (x + 2.4) + (2*x)*x)) + ((1*x)*(x*x))");
+        ArrayList<Token> tokens = lexer.lexAnalysis();
+        System.out.println(tokens.toString());
+        for (Token token : tokens)
+            System.out.println(token);
+    }
     // TODO add methods you think will be useful.
 }
--- src/differentiator/Token.java
+++ src/differentiator/Token.java
@@ -5,45 +5,40 @@ package differentiator;
  */
 public class Token {
     /**
-     * All the types of tokens that can be made.
-     * (*\d*[0-9]*[\+\*]+*[a-z]*
-     * Operation :+,*, var : [a-z]+
-     * number : integer and floating point, paren,
-     * DiffExp::= Expression (Op Expression)*
-     * Value::= Integer|Float|Variable
-     * Expression ::= Paren Value | Value Op Value Paren
-     * Op ::=  Plus|Mul
-     * Integer ::= [0-9]+
-     * Float ::= Interger.Integer
-     * Variable ::= [a-z]+
-     * Plus ::= +
-     * Mul ::= *
-     * Paren ::= (+|)+
+     * All the types of tokens that can be made. (*\d*[0-9]*[\+\*]+*[a-z]*
+     * Operation :+,*, var : [a-z]+ number : integer and floating point, paren,
+     * DiffExp::= Expression (Op Expression)* Value::= Integer|Float|Variable
+     * Expression ::= Paren Value | Value Op Value Paren Op ::= Plus|Mul Integer
+     * ::= [0-9]+ Float ::= Interger.Integer Variable ::= [a-z]+ Plus ::= + Mul
+     * ::= * Paren ::= (+|)+
      */
     public static enum Type {
-        Integer,
-        Float,
-        Variable,
-        Plus,
-        Mul,
-        Value(Integer|Float|Variable), 
-        Expression,
-        Op,
-        Paren,
-        DiffExp(Paren);
-        
+        FLOAT("\\d*\\.\\d+"), INTEGER("\\d+"), VARIABLE("[a-z]+"),
+        OPERATION("[+|*]"), WHITESPACE("[ ]+"), PAREN("[(|)]");
+
         private final String pattern;
-        private final Token.Type type;
-        
-        private Type(String pattern){
+
+        private Type(String pattern) {
             this.pattern = pattern;
         }
-        
-        private Type(Token.Type type){
-            this.type = type;
+
+        public String getPattern() {
+            return pattern;
         }
+
     }
-    
+
+    private final Type type;
+    private final String pattern;
+
+    public Token(Type type, String pattern) {
+        this.type = type;
+        this.pattern = pattern;
+    }
+
+    public String toString() {
+        return String.format("%s: %s", this.type.name(), this.pattern);
+    }
 
     // TODO write me
 }
